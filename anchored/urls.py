4cbdd5b6f0b17c6802aca5a07553bc00fa4a31ac
Ryland <rylandmathews1@gmail.com>
2023-06-06 11:45:53 -0400

    remove analog fails

--- anchored/urls.py
+++ anchored/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.landing, name='anchored'),
     path('vwaps/', views.get_daily_equity, name='vwaps'),
-    path('analogs/', views.get_analogs, name='analogs')
 ]
